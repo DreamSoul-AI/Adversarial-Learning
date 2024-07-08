2a0f8f0b4276df0e49bce83793534899089f86c5
AllenZhaoHang <allenzhao0517@gmail.com>
2024-07-08 17:08:13 +0800

    move the directory

--- Adversarial-Learning/OOD-Research-Pipe/clear.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -r data
-rm -r __pycache__
-rm ./output/attacked_data/*.pt
--- Adversarial-Learning/OOD-Research-Pipe/make_dec_eval.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python make_scripts_to_eval_detectors.py --run detectors_eval --mode base --num_gpus 1 
